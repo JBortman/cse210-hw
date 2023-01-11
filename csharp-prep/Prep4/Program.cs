de11c6e4bec786f1be487303ef145658bc14ad03
JBortman <jbortman33@gmail.com>
2023-01-11 00:56:36 -0800

    Prep 3 & 4 complete

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+        int input = 1;
+
+        while (input != 0)
+        {
+            Console.Write("Enter number: ");
+            input = int.Parse(Console.ReadLine());
+            numbers.Add(input);
+        }
+        //Get the sum of the list//
+        int total = 0;
+        float count = 0;
+        foreach (int i in numbers)
+        {
+            count++;
+            total += i;
+        }
+
+        //Calculate the average//
+        float average = total / (count - 1);
+
+        //Calculate largest number//
+        int large = numbers[0];
+        for (int l = 1;
+                l < count; l++)
+            if (numbers[l] > large)
+                large = numbers[l];
+
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {large}");
     }
 }
