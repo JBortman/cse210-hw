9440e2125192577d7976f89bb73e04a357321751
JBortman <jbortman33@gmail.com>
2023-01-11 01:44:58 -0800

    Prep3,4,5 Complete

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int favNumber = PropmtUserNumber();
+        int favSquared = SquareNumber(favNumber);
+
+        DisplayResult(userName, favSquared);
+
+        //Welcome function//
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+
+        }
+
+        //Prompt name//
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        //Favorite number//
+        static int PropmtUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+
+        //Square Number//
+        static int SquareNumber(int number)
+        {
+            int squared = number * number;
+            return squared;
+        }
+
+        //Display the result//
+        static void DisplayResult(string name, int squared)
+        {
+            Console.WriteLine($"{name}, the square root of your favorite number is {squared}");
+        }
+
     }
 }
