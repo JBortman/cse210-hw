5df3953caacdc768dbc7ba5ac7a510ead23fb31f
JBortman <jbortman33@gmail.com>
2023-01-11 00:04:27 -0800

    Prep3 Complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number?");
+        Random rnd = new Random();
+        int magicNum = rnd.Next(1, 101);
+
+        int userInput = 0;
+        while (userInput != magicNum)
+        {
+            Console.Write("What is your guess? ");
+            string input = Console.ReadLine();
+            userInput = int.Parse(input);
+
+            if (magicNum == userInput)
+            { Console.WriteLine("That is correct!"); }
+
+            else if (magicNum < userInput)
+            { Console.WriteLine("Go lower"); }
+
+            else if (magicNum > userInput)
+            { Console.WriteLine("Go higher"); }
+
+            else
+            { Console.WriteLine("Input not recognized"); }
+        }
+
     }
 }
