c3ed1de6e53e12ea88e5d0cf91f0075ba29afb11
JBortman <jbortman33@gmail.com>
2023-01-27 22:37:23 -0800

    More prompts added

--- prove/Develop02/PromptGen.cs
+++ prove/Develop02/PromptGen.cs
@@ -4,7 +4,23 @@ public class PromptGen
     //Default list options--------------------
     public string GenPrompt()
     {//Manual list
-        List<string> _prompts = new List<string> { "Who was the most interesting person I interacted with today?", "What was the best part of my day?", "How did I see the hand of the Lord in my life today?", "What was the strongest emotion I felt today?", "If I had one thing I could do over today, what would it be?" };
+        List<string> _prompts = new List<string>
+        { "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?",
+        "What did you do today?",
+        "Pick a random scripture. What does it mean to you?",
+        "If you're posterity saw this entry, what would you want them to know about you?",
+        "What new thing did you learn today?",
+        "What was your favorite song that you heard today?",
+        "Who was on your mind the most today?",
+        "Tough day huh? Tell me all about it.",
+        "How are you REALLY doing today?",
+        "What are you thinking about literally right now?",
+        "Why do you still write in your journal?"
+        };
         Random _rnd = new Random();
         int _rngIndex = _rnd.Next(_prompts.Count);
         string _random = _prompts[_rngIndex];
