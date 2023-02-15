4ec3b7d234e878b94799595ab2e398eeefbd335b
JBortman <jbortman33@gmail.com>
2023-02-15 00:10:23 -0800

    Final step complete

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -6,12 +6,16 @@ class Program
     {
         Assignment a1 = new Assignment("Samuel Bennet", "Multiplication");
         Console.WriteLine(a1.GetSummary());
-        Console.WriteLine("---------------");
+        Console.WriteLine("---------------");//Separator to see each section better
 
         MathAssignment a2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
         Console.WriteLine(a2.GetSummary());
         Console.WriteLine(a2.GetHomeworkList());
-        Console.WriteLine("---------------");
+        Console.WriteLine("---------------");//Separator to see each section better
+
+        WritingAssignment a3 = new WritingAssignment("Mary Waters","European History","The Causes of Wolrd War II");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
 
     }
 }
