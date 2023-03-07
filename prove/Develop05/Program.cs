bc03f256ea1acbdba55f3a148b2ea0b4dea214be
JBortman <jbortman33@gmail.com>
2023-03-06 23:09:42 -0800

    Main menu skeleton created

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,67 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        string select=null;
+
+        Console.Clear();
+        Console.WriteLine("Welcome to Eternal Quest! Select from the menu below");
+        Console.WriteLine("and begin your journey! Or load a previous journey");
+        Console.WriteLine("and continue where you left off.");
+        Console.WriteLine("");
+        while (select != "6")
+        {
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("1. Create New Goal");
+            Console.WriteLine("2. List Current Goals");
+            Console.WriteLine("3. Save Goals");
+            Console.WriteLine("4. Load Previous Goals");
+            Console.WriteLine("5. Mark Complete");
+            Console.WriteLine("6. Quit");
+            Console.Write("Your choice --> ");
+            select = Console.ReadLine();
+
+            if (select == "1")
+            {
+                Console.WriteLine("Create");
+            }
+            else if (select == "2")
+            {
+                Console.WriteLine("List");
+            }
+            else if (select == "3")
+            {
+                Console.WriteLine("Save");
+            }
+            else if (select == "4")
+            {
+                Console.WriteLine("Load");
+            }
+            else if (select == "5")
+            {
+                Console.WriteLine("Mark");
+            }
+            else if (select == "6")
+            {
+                string saveProg;
+                Console.Write("Save your progress (y/n) --> ");
+                saveProg = Console.ReadLine();
+                if (saveProg == "y"){
+                    Console.WriteLine("Progress saved");
+                    Console.WriteLine();
+                }
+                else if (saveProg == "n") {
+                Console.WriteLine();
+                }
+
+                Console.WriteLine("Your journey, has ended!");
+                Console.WriteLine();
+                break;
+            }
+
+            else {
+                Console.WriteLine("Invalid Entry, only select 1-6");
+            }
+
+        }
     }
 }
